3df562719c1123ba9f46fc49cdbb46b3e8fa898d
Geeth <induwarakwg3573@gmail.com>
2024-02-11 19:51:41 +0530

    Tutorial 5 completed

--- react-tutorial/src/App.jsx
+++ react-tutorial/src/App.jsx
@@ -1,19 +1,23 @@
 import "./App.css";
+import Home from "./Home";
+import Navbar from "./Navbar";
 
 function App() {
-  const title = "Hello World!";
-  const number = 50;
-  const link = "http://www.google.com";
+  // const title = "Hello World!";
+  // const number = 50;
+  // const link = "http://www.google.com";
   return (
     <div className="app">
+      <Navbar />
       <div className="content">
-        <h1>{title}</h1>
+        {/* <h1>{title}</h1>
         <p>{number}</p>
         <p>{"Hello Hello"}</p>
         <p>{[1, 2, 3]}</p>
         <p>{Math.random() * 20}</p>
 
-        <a href={link}>Google </a>
+        <a href={link}>Google </a> */}
+        <Home />
       </div>
     </div>
   );
