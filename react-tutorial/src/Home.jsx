35d4a9a74626e7aea2632101da2e5c37e8f5ff0c
Geeth <induwarakwg3573@gmail.com>
2024-02-12 11:13:36 +0530

    Tutorial 8 completed

--- react-tutorial/src/Home.jsx
+++ react-tutorial/src/Home.jsx
@@ -1,5 +1,23 @@
-const Home = () => {
-  return <div>Home Page</div>;
-};
+import { useState } from "react";
 
+const Home = () => {
+  // let name = 'mario';
+  const [name, setName] = useState('mario');
+  const [age, setAge] = useState(25);
+
+  const handleClick = () => {
+    // name = 'luigi';
+    setName('luigi');
+    setAge(30);
+  }
+
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <p>{ name } is { age } years old</p>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+}
+ 
 export default Home;
